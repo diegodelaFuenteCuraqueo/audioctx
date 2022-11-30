0689b41852cb6aa4be9da7b55853a8c50777ad70
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2022-11-30 02:06:53 -0300

    from singleton to instace

--- audioContextModule.js
+++ audioContextModule.js
@@ -1,73 +1,53 @@
-let audioModuleState = null;
-let audioModuleInstance = null;
 
-export const getAudioHandler = () => {
-  function getData() {
-    return audioModuleState;
+export const getAudioHandler = (audioElem) => {
+  const audioModuleState = {
+    audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+    audioElement: audioElem,
+    mediaStreamSrc: null,
+    analyser: null
   }
 
-  function init(audioElem) {
-    if (!audioModuleInstance) {
-      audioModuleState = {
-        audioContext: new (window.AudioContext || window.webkitAudioContext)(),
-        audioElement: audioElem,
-        mediaStreamSrc: null,
-        analyser: null
-      };
-      audioModuleInstance = {
-        getData,
-        getAmplitudeData,
-        getFrequencyData
-      };
-      _config();
-    }
+  function getState() {
+    return audioModuleState
   }
 
-  function _config() {
+  function config() {
     audioModuleState.mediaStreamSrc = audioModuleState.audioContext.createMediaElementSource(
       audioModuleState.audioElement
-    );
+    )
     audioModuleState.mediaStreamSrc.connect(
       audioModuleState.audioContext.destination
-    );
-    audioModuleState.analyser = audioModuleState.audioContext.createAnalyser();
+    )
+    audioModuleState.analyser = audioModuleState.audioContext.createAnalyser()
     audioModuleState.analyser.ampSize = 1024;
-    audioModuleState.mediaStreamSrc.connect(audioModuleState.analyser);
+    audioModuleState.mediaStreamSrc.connect(audioModuleState.analyser)
   }
 
   function getAmplitudeData() {
     if (audioModuleState.analyser) {
-      const data = new Uint8Array(512);
-      audioModuleState.analyser.getByteTimeDomainData(data);
+      const data = new Uint8Array(512)
+      audioModuleState.analyser.getByteTimeDomainData(data)
       const normalized = Array.from(data).map(sample => (sample / 255) - 0.5)
-      window.ampData = normalized;
-      return normalized;
+      window.ampData = normalized
+      return normalized
     }
   }
 
   function getFrequencyData() {
     if (audioModuleState.analyser) {
-      const data = new Uint8Array(512);
-      audioModuleState.analyser.getByteFrequencyData(data);
-      //const normalized = data.map(sample => (sample / 255))
-      window.freqData = data //normalized;
-      return data //normalized;
+      const data = new Uint8Array(512)
+      audioModuleState.analyser.getByteFrequencyData(data)
+      const normalized =Array.from(data).map(sample => (sample / 255))
+      window.freqData = normalized
+      return normalized
     }
-  }
-
-  function getInstance() {
-    if (!audioModuleInstance) {
-      throw new Error("Not initialized");
-    }
-    return audioModuleInstance;
   }
 
   return {
-    init,
-    getInstance,
-    getData,
+    config,
+    getState,
     getAmplitudeData,
     getFrequencyData
-  };
-};
+  }
+}
 
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { getAudioHandler } from "./audioContextModule.js";
 
 console.log("index.js loaded");
-
 const auElem = document.getElementById("audioElement");
-getAudioHandler().init(auElem);
-window.audioHandler = getAudioHandler().getInstance();
+//getAudioHandler().init(auElem);
+window.audioHandler = getAudioHandler(auElem)
+window.audioHandler.config()
 
 console.log(window.audioHandler.getData());
 
